d40a2727f592d4ecdcb14dbb8f5370e1bc26691f
Male Michael <malmike21@gmail.com>
2025-01-01 10:12:29 +0100

    fmt terraform

--- modules/vm-my-rides/outputs.tf
+++ modules/vm-my-rides/outputs.tf
@@ -1,4 +1,4 @@
 output "droplet_ip_address" {
-  value = digitalocean_droplet.vm-my-rides.ipv4_address
+  value     = digitalocean_droplet.vm-my-rides.ipv4_address
   sensitive = false # set to true if the logs will be publicly available
 }
--- modules/vm-my-rides/variables.tf
+++ modules/vm-my-rides/variables.tf
@@ -4,44 +4,44 @@ locals {
 
 variable "do_token" {
   description = "Digital Ocean personal access token"
-  sensitive = true
-  type = string
+  sensitive   = true
+  type        = string
 }
 
 variable "pvt_key" {
   description = "SSH private key on host machine"
-  sensitive = true
-  type = string
+  sensitive   = true
+  type        = string
 }
 
 variable "pub_key" {
   description = "SSH public key on host machine"
-  sensitive = false
-  type = string
+  sensitive   = false
+  type        = string
 }
 
 variable "do_ssh_key_name" {
   description = "Name of ssh key store in digital ocean"
-  sensitive = false
-  type = string
+  sensitive   = false
+  type        = string
 }
 
 variable "ubuntu_image" {
   description = "Version of ubuntu to be installed"
-  type = string
+  type        = string
 }
 
 variable "droplet_name" {
   description = "Name of droplet"
-  type = string
+  type        = string
 }
 
 variable "deployment_region" {
   description = "Region the droplet is to be created"
-  type = string
+  type        = string
 }
 
 variable "droplet_size" {
   description = "Size (Compute specifications) of the droplet"
-  type = string
+  type        = string
 }
